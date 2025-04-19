4024ccb33cfd0c458d3251cbde0878f5e59c0b6a
Siva-Muniyandi <msivalearning@gmail.com>
2025-04-19 09:31:40 +0530

    Update UITest.java

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class UITest {
 	@Test
 	public void startBrowser() {
+		System.out.println("Starting Test");
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://naveenautomationlabs.com/");
 		Assert.assertEquals(driver.getTitle().contains("Welcome to Naveen AutomationLabs - naveen automationlabs"),
